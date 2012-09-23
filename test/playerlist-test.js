4068b75ed33eaf2e49a2fb4cfe442987704c96d4
Matt Biggin <Matthew.Biggin@gmail.com>
2012-09-23 20:58:08 +0100

    Initial call to API now working

--- test/playerlist-test.js
+++ test/playerlist-test.js
@@ -1,8 +1,12 @@
 var bf3stats = require("../index.js").platform(360);
 
-var players = ["ultraflynn", "R1ckyDaMan19"];
+// This should work but produces this (which is wrong):
+// players=ultraflynn&players=R1ckyDaMan19
+var playersArray = ["ultraflynn", "R1ckyDaMan19"];
+var playersList = "ultraflynn,R1ckyDaMan19";
+
 var opt = null;
-bf3stats.playerlist(players, opt, function(err, list, failed) {
+bf3stats.playerlist(playersList, opt, function(err, list, failed) {
 	if (err) {
 		console.log(err);
 	} else {
