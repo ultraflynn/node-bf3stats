11d437cc958851523e04e9c57609658c64d981c7
Matt Biggin <Matthew.Biggin@gmail.com>
2012-09-23 16:33:10 +0100

    Started work on playerlist

---
+++ index.js
@@ -0,0 +1,16 @@
+//
+// BF3 Stats API for Node.js
+//
+var bf3stats = require("./lib/bf3stats");
+
+function InvalidPlatformException() {
+}
+
+exports.platform = function(platform) {
+	if (!(platform === "PC" || 
+		platform === "360" || platform === 360 ||
+		platform === "ps3")) {
+		throw new Error("Invalid platform '" + platform + "'. Should be PC, 360 or ps3.");
+	}
+	return new bf3stats.createFor(platform);
+};
---
+++ test/playerlist-test.js
@@ -0,0 +1,11 @@
+var bf3stats = require("../index.js").platform(360);
+
+var players = ["ultraflynn", "R1ckyDaMan19"];
+var opt = null;
+bf3stats.playerlist(players, opt, function(err, list, failed) {
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("list", list, "failed", failed);
+	}
+});
