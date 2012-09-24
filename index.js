99f6e048ea4b961501837591ee3a36c57452664d
Matt Biggin <Matthew.Biggin@gmail.com>
2012-09-24 18:10:05 +0100

    Added the rest of the API calls

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.platform = function(platform) {
 	if (!(platform === "PC" || 
 		platform === "360" || platform === 360 ||
 		platform === "ps3")) {
-		throw new Error("Invalid platform '" + platform + "'. Should be PC, 360 or ps3.");
+		throw new Error("Invalid platform '" + platform + "'. Options are PC, 360 or ps3.");
 	}
 	return new bf3stats.createFor(platform);
 };
