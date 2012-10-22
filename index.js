885b6d32343d3caa97eb0dae3d932c897ff06d13
Matt Biggin <Matthew.Biggin@gmail.com>
2012-10-22 20:28:31 +0100

    Refactor to better support caching

--- index.js
+++ index.js
@@ -4,11 +4,16 @@
 var core = require("./lib/bf3stats");
 var cache = require("./lib/bf3stats-cache");
 
+// Creates an instance of the Core API
+//
+// platform: PC, 360 or ps3
+// ident: client specific
+// cacheStrategy: leave blank for default or see docs
 exports.platform = function(platform, ident, cacheStrategy) {
 	if (!(platform === "PC" || 
 		platform === "360" || platform === 360 ||
 		platform === "ps3")) {
 		throw new Error("Invalid platform '" + platform + "'. Options are PC, 360 or ps3.");
 	}
-	return new core.createFor(platform, ident, cache.newInstance(cacheStrategy));
+	return core.newInstance(platform, ident, cache.newInstance(cacheStrategy));
 };
